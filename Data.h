f5cc212ba04a8ea76f69c996522e8ffae994e1b2
ricardoyang00 <ricardo.yang00@gmail.com>
2023-12-11 14:30:01 +0000

    Data header implemented

--- Data.h
+++ Data.h
@@ -1,14 +1,40 @@
-//
-// Created by chill on 11-12-2023.
-//
-
-#ifndef UNTITLED_DATA_H
-#define UNTITLED_DATA_H
+#ifndef AED_AEROPORTO_DATA_H
+#define AED_AEROPORTO_DATA_H
 
 
-class Data {
+class Airport {
+public:
+    std::string code;
+    std::string name;
+    std::string city;
+    std::string country;
+    double latitude;
+    double longitude;
 
+    Airport(const std::string& c, const std::string& n, const std::string& ci, const std::string& co, double lat, double lon)
+            : code(c), name(n), city(ci), country(co), latitude(lat), longitude(lon) {}
+};
+
+class Airline {
+public:
+    std::string code;
+    std::string name;
+    std::string callsign;
+    std::string country;
+
+    Airline(const std::string& c, const std::string& n, const std::string& cs, const std::string& co)
+            : code(c), name(n), callsign(cs), country(co) {}
+};
+
+class Flight {
+public:
+    std::string source;
+    std::string target;
+    std::string airlineCode;
+
+    Flight(const std::string& src, const std::string& tgt, const std::string& ac)
+            : source(src), target(tgt), airlineCode(ac) {}
 };
 
 
-#endif //UNTITLED_DATA_H
+#endif //AED_AEROPORTO_DATA_H
--- ReadData.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by chill on 11-12-2023.
-//
-
-#include "ReadData.h"
