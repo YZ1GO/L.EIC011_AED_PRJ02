3c281674ca81d5ea507f34d0199eb9b61dd2bf41
ricardoyang00 <ricardo.yang00@gmail.com>
2023-12-19 22:01:10 +0000

    graph implemented & output data implemented

--- OutputData.cpp
+++ OutputData.cpp
@@ -1,49 +1,5 @@
 #include "OutputData.h"
 
-
-/*void convertAirportsToTextFile(const Graph<Airport>& airportGraph, const std::string& filename) {
-    std::ofstream outFile(filename);
-    if (!outFile.is_open()) {
-        std::cerr << "Error: Unable to open file " << filename << std::endl;
-        return;
-    }
-
-    for (const auto& a : airportGraph.getVertexSet()) {
-        auto airport = a->getInfo();
-        outFile << ">> [" << airport.getCode() << "] " << airport.getName() << " <<" << std::endl;
-        outFile << "    City       : " << airport.getCity() << std::endl;
-        outFile << "    Country    : " << airport.getCountry() << std::endl;
-        outFile << "    Coordinates: (" << airport.getLocation().latitude << ", " << airport.getLocation().longitude << ")" << std::endl;
-        outFile << "    Flights    : " << std::endl;
-        outFile << "    From this airport: " << a->getOutdegree() << std::endl;
-        outFile << "    To this airport: " << a->getIndegree() << std::endl;
-        for (auto e : a->getAdj()) {
-            auto target = e.getDest()->getInfo();
-            outFile << airport.getCode() << " -> " << target.getCode() << " by " << e.getAirlineCode() << " airline : " << e.getDistance() << "km" << std::endl;
-        }
-        outFile << std::endl;
-    }
-    outFile.close();
-}
-
-void convertAirlinesToTextFile(const Graph<Airline>& airlineGraph, const std::string& filename) {
-    std::ofstream outFile(filename);
-    if (!outFile.is_open()) {
-        std::cerr << "Error: Unable to open file " << filename << std::endl;
-        return;
-    }
-
-    for (const auto& al : airlineGraph.getVertexSet()) {
-        auto airline = al->getInfo();
-        outFile << ">> [" << airline.getCode() << "] " << airline.getName() << " <<" << std::endl;
-        outFile << "    Callsign : " << airline.getCallsign() << std::endl;
-        outFile << "    Country  : " << airline.getCountry() << std::endl;
-        outFile << std::endl;
-    }
-
-    outFile.close();
-}*/
-
 void convertDataGraphToTextFile(const Graph<Airport>& airportGraph, const std::string& filename) {
     std::ofstream outFile(filename);
     if (!outFile.is_open()) {
@@ -58,14 +14,15 @@ void convertDataGraphToTextFile(const Graph<Airport>& airportGraph, const std::s
         outFile << "    Country    : " << airport.getCountry() << std::endl;
         outFile << "    Coordinates: (" << airport.getLocation().latitude << ", " << airport.getLocation().longitude << ")" << std::endl;
         outFile << "    Flights from this airport : " << a->getOutdegree() << std::endl;
-        outFile << "    Flights to this airport   : " << a->getIndegree() << std::endl;
+        outFile << "    Flights to this airport   : " << a->getIndegree() << std::endl << std::endl;
 
         for (auto e : a->getAdj()) {
             auto target = e.getDest()->getInfo();
-            outFile << airport.getCode() << " -> " << target.getCode() << " : " << e.getDistance() << "km" << std::endl;
-            outFile << "by Airlines: " << std::endl;
+            outFile << "    >>" << airport.getCode() << " -> " << target.getCode() << " : " << e.getDistance() << "km" << std::endl;
+            outFile << "        by Airlines: " << std::endl;
+            int i = 0;
             for (auto airline : e.getAirlines()) {
-                outFile << "[" << airline.getCode() << "] " << airline.getCallsign() << std::endl;
+                outFile << "             " << i++ << ".[" << airline.getCode() << "] " << airline.getCallsign() << std::endl;
             }
             outFile << std::endl;
         }
