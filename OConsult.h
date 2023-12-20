868cd96b8957317024645f8f00189f0c2e80efb0
ricardoyang00 <ricardo.yang00@gmail.com>
2023-12-20 19:51:40 +0000

    smallest number of stops between 2 airports

--- OConsult.h
+++ OConsult.h
@@ -5,12 +5,16 @@
 #include "ParseData.h"
 #include <unordered_set>
 #include <unordered_map>
+#include <limits>
 
 class OConsult {
 private:
     const Graph<Airport>& consultGraph;
 
     void dfs_art(Vertex<Airport> *v, stack<string> &s, unordered_set<string> &l, int &i);
+    void dfs_greatestTrip(Vertex<Airport>* source, Vertex<Airport>* target, vector<vector<Airport>>& greatestTrips, vector<Airport> currentTrip, int& maxTrip);
+    void dfs_smallestTrip(Vertex<Airport>* source, Vertex<Airport>* target, vector<vector<Airport>>& smallestTrips, vector<Airport> currentTrip, int& minTrip);
+
     vector<pair<Airport,int>> topTrafficCapacityAirports();
 
 
@@ -20,7 +24,7 @@ public:
     unordered_set<string> searchEssentialAirports();
     vector<pair<Airport,int>> searchTopKairportGreatestAirTrafficCapacity(const int& k);
     vector<vector<Airport>> searchTripGreatestNumberOfStopsBetweenThem(const Airport& source, const Airport& target);
-    void dfs_greatestTrip(Vertex<Airport>* source, Vertex<Airport>* target, vector<vector<Airport>>& greatestTrips, vector<Airport> currentTrip, int& maxTrip);
+    vector<vector<Airport>> searchTripSmallestNumberOfStopsBetweenThem(const Airport& source, const Airport& target);
 
     int searchNumberOfFlightsOutOfAirport(const Airport& airport);
     int searchNumberOfFlightsToAirport(const Airport& airport);
