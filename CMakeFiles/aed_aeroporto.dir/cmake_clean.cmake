9b1942e007302917df673ca960f4ad87f886a95d
ricardoyang00 <ricardo.yang00@gmail.com>
2023-12-26 18:23:08 +0000

    documentation for Airport's class & deleted Data.cpp file

---
+++ CMakeFiles/aed_aeroporto.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aed_aeroporto.dir/Consult.cpp.o"
+  "CMakeFiles/aed_aeroporto.dir/Consult.cpp.o.d"
+  "CMakeFiles/aed_aeroporto.dir/Data.cpp.o"
+  "CMakeFiles/aed_aeroporto.dir/Data.cpp.o.d"
+  "CMakeFiles/aed_aeroporto.dir/ListAirports.cpp.o"
+  "CMakeFiles/aed_aeroporto.dir/ListAirports.cpp.o.d"
+  "CMakeFiles/aed_aeroporto.dir/OutputData.cpp.o"
+  "CMakeFiles/aed_aeroporto.dir/OutputData.cpp.o.d"
+  "CMakeFiles/aed_aeroporto.dir/ParseData.cpp.o"
+  "CMakeFiles/aed_aeroporto.dir/ParseData.cpp.o.d"
+  "CMakeFiles/aed_aeroporto.dir/Script.cpp.o"
+  "CMakeFiles/aed_aeroporto.dir/Script.cpp.o.d"
+  "CMakeFiles/aed_aeroporto.dir/Utilities.cpp.o"
+  "CMakeFiles/aed_aeroporto.dir/Utilities.cpp.o.d"
+  "CMakeFiles/aed_aeroporto.dir/main.cpp.o"
+  "CMakeFiles/aed_aeroporto.dir/main.cpp.o.d"
+  "aed_aeroporto"
+  "aed_aeroporto.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/aed_aeroporto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
